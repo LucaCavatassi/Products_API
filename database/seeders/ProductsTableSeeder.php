9e4152ad1ff25c9d787ce76a88e2fa358ba90d70
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-23 15:54:03 +0200

    feat: create seeder product

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i <= 100; $i++) {
+            $newProduct = new Product();
+            $newProduct->name = $faker->word(3, true);
+            $newProduct->description = $faker->sentence();
+            $newProduct->price = $faker->randomFloat(2, 3, 10000);
+            $newProduct->available = $faker->boolean();
+            $newProduct->quantity = $faker->randomNumber(2, false);
+        }
+    }
+}
